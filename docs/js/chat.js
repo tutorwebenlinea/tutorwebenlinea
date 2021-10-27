d2b151eb8c551ab68b06a8f0784392d433622234
caleman9791 <caleman9791@protonmail.com>
2021-10-27 15:24:54 -0400

    ok

--- docs/js/chat.js
+++ docs/js/chat.js
@@ -4,7 +4,7 @@ var Tawk_API = Tawk_API || {},
 	var s1 = document.createElement("script"),
 		s0 = document.getElementsByTagName("script")[0];
 	s1.async = true;
-	s1.src = 'https://embed.tawk.to/61794403f7c0440a59203d23/1fj0r3cce';
+	s1.src = 'https://embed.tawk.to/6179a751f7c0440a59204d90/1fj1jb6b5';
 	s1.charset = 'UTF-8';
 	s1.setAttribute('crossorigin', '*');
 	s0.parentNode.insertBefore(s1, s0);
